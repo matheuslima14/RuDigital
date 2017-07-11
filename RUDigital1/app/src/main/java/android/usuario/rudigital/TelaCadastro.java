95889619f4682bd4679dddea51caba83acdf0e54
Pedro Ferreira Junio Duarte <pedrofjduarte@gmail.com>
2017-07-10 22:49:08 -0300

    Alterações na tela de login e cadastro de usuário
    A tela de login agora vai receber os dados: matrícula e senha. A tela
    cadastro foi alterada para receber os dados combinados no projeto. A
    funcionalidade cadastrar usuário está funcionando, cadastrando no banco
    de dados remoto.

--- RUDigital1/app/src/main/java/android/usuario/rudigital/TelaCadastro.java
+++ RUDigital1/app/src/main/java/android/usuario/rudigital/TelaCadastro.java
@@ -1,24 +1,27 @@
 package android.usuario.rudigital;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class TelaCadastro extends AppCompatActivity {
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
-    EditText editNome, editEmail2, editSenha2;
-    Button btnCancelar, btnRegistar;
+public class TelaCadastro extends AppCompatActivity implements View.OnClickListener {
 
-    String url = "";
-    String parametros = "";
+    private EditText editNome, editEmail2, editRG, editMatriculaSiape, editSenha2, editConfirmaSenha;
+    private Button btnRegistar, btnCancelar;
+
+    private OkHttpClient okHttpClient;
+    private Request request;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,83 +30,70 @@ public class TelaCadastro extends AppCompatActivity {
 
         editNome = (EditText) findViewById(R.id.editNome);
         editEmail2 = (EditText) findViewById(R.id.editEmail2);
+        editRG = (EditText) findViewById(R.id.editRG);
+        editMatriculaSiape = (EditText) findViewById(R.id.editMatriculaSiape);
         editSenha2 = (EditText) findViewById(R.id.editSenha2);
+        editConfirmaSenha = (EditText) findViewById(R.id.editConfirmaSenha);
         btnCancelar = (Button) findViewById(R.id.btnCancelar);
         btnRegistar = (Button) findViewById(R.id.btnRegistrar);
 
-        btnCancelar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-
-        btnRegistar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ConnectivityManager connMgr = (ConnectivityManager)
-                        getSystemService(Context.CONNECTIVITY_SERVICE);
-                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-                if (networkInfo != null && networkInfo.isConnected()) {
-
-
-                    String nome = editNome.getText().toString();
-                    String email = editEmail2.getText().toString();
-                    String senha = editSenha2.getText().toString();
-
-                    if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
-                        Toast.makeText(getApplicationContext(), "Nenhum campo pode estar vazio", Toast.LENGTH_LONG).show();
-                    } else {
-                        url = "http://192.168.0.105:80/login/registrar .php";
-
-                        parametros = "nome=" + nome + "&email=" + email + "&senha=" + senha;
-
-                        new SolicitaDados().execute(url);
-                    }
-
-
-                } else {
-                    Toast.makeText(getApplicationContext(), "Nenhuma conexão foi detectata", Toast.LENGTH_LONG).show();
-                }
-
-
-            }
-        });
-
-    }
-
-    private class SolicitaDados extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... urls) {
-
-
-            return Conexao.postDados(urls[0], parametros);
-
-        }
-
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String resultado) {
-
-            if (resultado.contains("email_erro")) {
-                Toast.makeText(getApplicationContext(), "Este email já está cadastrado", Toast.LENGTH_LONG).show();
-
-            } else if (resultado.contains("registro_ok")) {
-                Toast.makeText(getApplicationContext(), "Registro concluído com sucesso", Toast.LENGTH_LONG).show();
-                Intent abreInicio = new Intent(TelaCadastro.this, TelaLogin.class);
-                startActivity(abreInicio);
-            } else {
-                Toast.makeText(getApplicationContext(), "Ocorreu um erro", Toast.LENGTH_LONG).show();
-
-            }
-        }
+        btnRegistar.setOnClickListener(this);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        finish();
+    public void onClick(View v) {
 
+        if (!(editSenha2.getText().toString().equals(editConfirmaSenha.getText().toString()))) {
+            Toast.makeText(getApplicationContext(), "Senhas não conferem!", Toast.LENGTH_SHORT).show();
+        }
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+
+
+                try {
+
+                    String nome = editNome.getText().toString();
+                    String email = editEmail2.getText().toString();
+                    String rg = editRG.getText().toString();
+                    String matriculasiape = editMatriculaSiape.getText().toString();
+                    String senha = editSenha2.getText().toString();
+                    String confirmaSenha = editConfirmaSenha.getText().toString();
+
+                    url = "HTTP://192.168.0.101:802/appRUDigital/CadastroUsuario.php?nome=" + nome + "&email=" + email + "&senha=" + senha + "&matriculasiape=" + matriculasiape + "&rg=" + rg;
+
+                    okHttpClient = new OkHttpClient();
+                    request = new Request.Builder().url(url).build();
+
+                    Response response = okHttpClient.newCall(request).execute();
+                    Log.i("MSG", response.body().string());
+
+                    //Toast.makeText(getApplicationContext(), "Cadastrado com Sucesso!", Toast.LENGTH_SHORT).show();
+                    //AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+                    //dlg.setMessage("Cadastrado com Sucesso!");
+                    //dlg.show();
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    //Toast.makeText(getApplicationContext(), "Erro no Cadastro!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        Thread threadCadastrar = new Thread(r);
+        threadCadastrar.start();
+    }
+
+
+    public void telaLogin(View v) {
+        Intent abreLogin = new Intent(this, TelaLogin.class);
+        startActivity(abreLogin);
     }
 }
+
+
+
+
+
