8d93c505b2080c756f626d13e324d6911b48f8d5
Pedro Ferreira Junio Duarte <pedrofjduarte@gmail.com>
2017-10-09 23:09:28 -0300

    Login Funcionando

--- RUDigital1/app/src/main/java/android/usuario/rudigital/TelaLogin.java
+++ RUDigital1/app/src/main/java/android/usuario/rudigital/TelaLogin.java
@@ -1,30 +1,28 @@
 package android.usuario.rudigital;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.gson.Gson;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class TelaLogin extends AppCompatActivity {
 
     private EditText editMatriculaSiape, editSenha1;
     private Button btnLogar;
     private TextView txtCadastro;
-
-    private OkHttpClient okHttpClient;
-    private Request request;
-    private String url;
+    private OkHttpClient client;
     Usuario usuario;
 
     @Override
@@ -53,58 +51,56 @@ public class TelaLogin extends AppCompatActivity {
                 } else if (editSenha1.getText().toString().isEmpty()) {
                     Toast.makeText(getApplicationContext(), "O campo senha está vazio!", Toast.LENGTH_SHORT).show();
                 } else {
-
-                    Runnable r = new Runnable() {
-                        @Override
-                        public void run() {
-
-                            try {
-
-                                String matriculasiape = editMatriculaSiape.getText().toString();
-                                String senha = editSenha1.getText().toString();
-
-                                url = "HTTP://172.19.6.31:802/appRUDigital/LoginUsuario.php?matriculasiape=" + matriculasiape + "&senha=" + senha;
-
-                                okHttpClient = new OkHttpClient();
-                                request = new Request.Builder().url(url).build();
-
-                                Runnable r = new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        try {
-
-                                            Response response = okHttpClient.newCall(request).execute();
-                                            Log.i("MSG", response.body().string());
-
-                                            Gson json = new Gson();
-                                            usuario = json.fromJson(response.body().string(), Usuario.class);
-
-                                            if (usuario != null) {
-                                                handler.sendEmptyMessage(0);
-                                            } else {
-                                                handler.sendEmptyMessage(1);
-                                            }
-
-                                        } catch (Exception e) {
-                                            handler.sendEmptyMessage(2);
-                                        }
-                                    }
-                                };
-                                Thread threadresposta = new Thread(r);
-                                threadresposta.start();
-
-                            } catch (Exception e) {
-                                //e.toString();
-                                handler.sendEmptyMessage(2);
-                                //e.printStackTrace();
-                            }
-                        }
-                    };
-                    Thread threadLogar = new Thread(r);
-                    threadLogar.start();
+                    client = new OkHttpClient();
+                    getWebService();
                 }
             }
         });
+    }
+
+    private void getWebService() {
+
+        String matriculasiape = editMatriculaSiape.getText().toString();
+        String senha = editSenha1.getText().toString();
+
+        final Request request = new Request.Builder().url("http://192.168.0.100:802/appRUDigital/LoginUsuario.php?matriculasiape=" + matriculasiape + "&senha=" + senha).build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                //falho
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, final Response response) throws IOException {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                try {
+
+                                    Gson json = new Gson();
+
+                                    usuario = json.fromJson(response.body().string(), Usuario.class);
+
+                                    if (usuario != null) {
+                                        handler.sendEmptyMessage(0);
+                                    } else {
+                                        handler.sendEmptyMessage(1);
+                                    }
+
+
+                                } catch (IOException ioe) {
+                                    handler.sendEmptyMessage(2);
+                                }
+                            }
+                        });
+                    }
+                });
     }
 
     private Handler handler = new Handler() {
